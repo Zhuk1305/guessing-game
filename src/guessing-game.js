24f877c54208775b74a64c13138e6a68e3ea2036
Zhuk1305 <vaneczhuk@gmail.com>
2022-08-14 09:02:36 +0300

    feat: add decision

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.root = null;
+    this.max = 0;
+    this.current = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.max = max;
+    this.current = Math.ceil(max / 2);
+    this.min = min;
+  }
 
-    }
+  guess() {
+    return this.current;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.current;
+    this.current = this.current - Math.floor((this.current - this.min) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.current;
+    this.current = this.current + Math.ceil((this.max - this.current) / 2);
+  }
 }
 
 module.exports = GuessingGame;
